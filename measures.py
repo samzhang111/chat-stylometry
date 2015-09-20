25027882280c7a360f07b283c1f6bd7d4c9664b9
samzhang111 <shimian.zhang@gmail.com>
2015-09-20 15:38:09 -0700

    More statistical features -- wip tests failing
    These tests are terrible

--- measures.py
+++ measures.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from collections import defaultdict
 import sys
 
+
 def hapax_legomena(wordcounts):
     unique_words = 0.0
     total_words = 0.0
@@ -35,6 +36,7 @@ def yules_i(wordcounts):
 
     return (m1 * m1) / (m2 - m1)
 
+
 def calculate_v(wordcounts):
     '''
     V(i, N) is the frequency of tokens appearing i times in a corpus of N total words.
@@ -53,10 +55,25 @@ def calculate_v(wordcounts):
 
     return V
 
+
 def simpsons_d(wordcounts, V):
     simpsons_d = 0.0
     total = float(sum(wordcounts.itervalues()))
+
     for counts in wordcounts.itervalues():
-        simpsons_d += V[counts] * (counts / total) * ( (counts-1)/(total-1) )
+        simpsons_d += V[counts] * (counts / total) * ((counts - 1) /
+                                                      (total - 1))
 
     return simpsons_d
+
+
+def sichels_s(wordcounts):
+    return hapax_dislegomena(wordcounts) / \
+           len(wordcounts)
+
+def brunets_w(wordcounts):
+    vocab_size = len(wordcounts)
+    corpus_size = sum(wordcounts.values())
+
+    return corpus_size ** (vocab_size ** -0.172)
+
--- measures_test.py
+++ measures_test.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 from pyspecs import *
 import measures
+import sys
 
 with given.wordcounts_for_a_document:
     wordcounts = {"the": 5, "horse": 2, "ran": 1}
@@ -44,4 +46,29 @@ with given.wordcounts_for_a_document:
 
             the(simpsons_d).should.equal(answer)
 
+    with when.calculating_sichels_s:
+        sichels_s = measures.sichels_s(wordcounts)
 
+        with then.it_should_equal_the_ratio_of_twice_occurring_words_to_the_vocab_size:
+            the(sichels_s).should.equal(measures.hapax_dislegomena(wordcounts) /
+                                        len(wordcounts))
+
+
+    with when.calculating_brunets_w:
+        brunets_w = measures.brunets_w(wordcounts)
+
+        with then.it_should_equal_the_brunets_w:
+            total = sum(wordcounts.values())
+            exponent = len(wordcounts) ** -0.172
+
+            the(brunets_w).should.equal(total ** exponent)
+
+
+
+    with when.calculating_honores_r:
+        V = measures.calculate_v(wordcounts)
+        honore_r = measures.honores_r(wordcounts, V)
+
+        with then.it_should_correctly_calculate_honores_r:
+            answer = 100 * log(sum(wordcounts.values())) / (1 - v[1]/len(wordcounts))
+            the(honores_r).should.equal(answer)
